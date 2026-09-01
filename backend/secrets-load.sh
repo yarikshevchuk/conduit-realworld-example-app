57ac235118a716ccc9bb7fbf10c6e5495cc8a01a
Yaroslav Shevchuk <yarikshevchuk06@gmail.com>
2026-09-01 16:03:46 +0300

    Docker - Added production env variables. Secrets load script

---
+++ backend/secrets-load.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+SECRET=$(
+  aws secretsmanager get-secret-value \
+  --secret-id = conduit/db/credentials \ 
+  --query SecretString --output text
+)
+
+export PROD_DB_USERNAME=$(echo "$SECRET" | jq -r .DB_USER)
+export PROD_DB_PASSWORD=$(echo "$SECRET" | jq -r .DB_PASSWORD)
+export PROD_DB_NAME=$(echo "$SECRET" | jq -r .DB_NAME)
+
+exec "$@"
